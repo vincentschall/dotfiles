1caecc9d89adc08a348bd0efbf0d9b4665afa85b
Vincent Schall <v.schall@unibas.ch>
2026-04-21 15:19:56 +0200

    fix treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,7 +28,7 @@ require("lazy").setup({
             "nvim-treesitter/nvim-treesitter",
             build = ":TSUpdate",
             config = function()
-                require("nvim-treesitter.configs").setup({
+                require("nvim-treesitter.config").setup({
                     ensure_installed = { "lua" },
                     highlight = { enable = true },
                 })
